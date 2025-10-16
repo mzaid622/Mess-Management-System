e63c8113fa592743220b84e15fbef4f4a10d864c
mzaid622 <m.zaid990447@gmail.com>
2025-10-16 11:12:34 +0500

    Add project files.

---
+++ Mess Management System/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mess_Management_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login_page()
+        {
+            return View();
+        }
+    }
+}
